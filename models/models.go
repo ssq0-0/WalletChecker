818e025230d52ea99f339dcf6a2bb3109f90b683
ssq0-0 <snikers1kitkat2@gmail.com>
2024-12-13 21:56:13 +0300

    Add new response model

--- models/models.go
+++ models/models.go
@@ -4,3 +4,9 @@ type LineaResp struct {
 	Address string `json:"user_address"`
 	Xp      int    `json:"xp"`
 }
+
+type OdosResp struct {
+	Data struct {
+		PendingTokenBalance string `json:"pendingTokenBalance"`
+	} `json:"data"`
+}
